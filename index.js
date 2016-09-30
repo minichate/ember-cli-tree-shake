7c27b7dc85638f818c3b788c0c6f39144375b7a1
Christopher Troup <ctroup@freshbooks.com>
2016-09-30 13:29:12 -0400

    First pass at shaking out dead computed properties

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 /* jshint node: true */
 'use strict';
 
+var cst = require('cst');
+var mime = require('mime-types');
+var fs = require('fs');
+var walk = require('fs-walk');
+var path = require('path');
+var DeadComputedProperties = require('./lib/dead-computed-properties');
+
 module.exports = {
-  name: 'ember-cli-tree-shake'
+  name: 'ember-cli-tree-shake',
+  includedCommands: function() {
+    var treePaths = this.treePaths;
+    var appPath = treePaths.app;
+
+    return {
+      treeShake: {
+        name: 'tree-shake',
+        description: 'Discover code that is probably dead or unused',
+        run: function(commandOptions, rawArgs) {
+          var parser = new cst.Parser();
+
+          walk.walkSync(appPath, function(basedir, filename, stat) {
+            var filepath = path.join(basedir, filename);
+            if (mime.lookup(filepath) !== 'application/javascript') {
+              return;
+            }
+
+            var code = fs.readFileSync(filepath, 'utf8');
+            var tree = parser.parse(code);
+
+            new DeadComputedProperties(tree, treePaths, filepath).emit();
+          });
+        }
+      }
+    }
+  }
 };
